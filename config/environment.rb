ac49688e1cdb72d1cd6fdca60369e2803f4e07db
Hunter Kassow <hunterkassow@protonmail.com>
2022-07-14 19:21:05 -0400

    heroku

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ require_all 'app'
 configure :production do 
     db = URL.parse(ENV[ 'postgres://wjdyalsgfxouzm:692ca0193b273ddf87c9257c48a23a14579753f91e42412136ebfbad8dd9b947@ec2-52-20-166-21.compute-1.amazonaws.com:5432/d1lrgudk1ghe3i'] || 'postgres://localhost.mydb')
 
-    AcviteRecord::Base.establish.connection (
+    AcviteRecord::Base.establish_connection (
         :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
         :host => db.host,
         :username => db.user,
